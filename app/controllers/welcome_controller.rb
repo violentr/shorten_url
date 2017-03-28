aac35d32d6a98037fd23ac05f84713a787bb799d
Deniss <den.b@itservicesgroup.co.uk>
2017-03-28 16:54:42 +0100

    Add logic for shorten_url, used SecureRandom lib
    Save short_url and original_url to db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index
+    @short_urls = ShortUrl.all.map(&:original_url)
   end
 
   def shorten_url
-
+    random = SecureRandom.hex
+    url = params[:short_url][:url] || ""
+    ShortUrl.create(short_url: random, original_url: url)
   end
 end

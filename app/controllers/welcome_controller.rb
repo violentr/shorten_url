285794d9d31584e3a6a0984138e46f93fb47c9b3
Deniss <den.b@itservicesgroup.co.uk>
2017-03-29 14:48:32 +0100

    Add Handle short_url to redirect to original_url
    Generated random string cut to 6 symbols,
    populate generated string on index page as link
    to original website, like:
    localhost:3000/KHL575HG
    where is KHL575HG is shorten url to original website

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,17 @@
 class WelcomeController < ApplicationController
 
   def index
-    @short_urls = ShortUrl.all.map(&:original_url)
+    @short_urls = ShortUrl.all
+    if params[:id]
+    original_url = ShortUrl.find_by(short_url: params[:id]).original_url
+    redirect_to "http://#{original_url}", :status => 301
+    end
   end
 
   def shorten_url
-    random = SecureRandom.hex
+    random = SecureRandom.hex.slice(0..5).upcase
     url = params[:short_url][:url] || ""
     ShortUrl.create(short_url: random, original_url: url)
+    redirect_to welcome_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index', as: :welcome
+  get '/:id', controller: 'welcome', action: 'index'
   post 'welcome/shorten_url'
+  post '/' , controller: 'welcome', action: 'shorten_url'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

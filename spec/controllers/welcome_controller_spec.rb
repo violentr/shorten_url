6cbf337e137e8afa749aa5d0d817de9f80d9ffea
Deniss <den.b@itservicesgroup.co.uk>
2017-03-30 00:05:54 +0100

    Add Controller spec for welcome
    Tests added checking that method returns
    JSON object and it is not empty, contains data.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,4 +1,3 @@
-
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
@@ -8,4 +7,26 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+   describe "POST #shorten_url" do
+    let(:url) { "www.google.co.uk" }
+
+    before do
+      post :shorten_url, params: {url: url}
+      @output = JSON.parse(response.body)
+    end
+
+    it "returns JSON object as output" do
+      expect(@output).to be_a(Hash)
+    end
+
+    it "returns object with keys" do
+      expect(@output.keys).to eq(%w(id short_url original_url))
+    end
+
+    it "should have data for original and short_url" do
+      expect(@output["original_url"]).to eq(url)
+      expect(@output["short_url"]).not_to be_blank
+      expect(@output["short_url"]).to be_a(String)
+    end
+   end
 end
